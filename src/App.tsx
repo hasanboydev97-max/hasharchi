dc624bd9a59912b78b6ef02fb2f909db1466a95c
hasanboydesigner-source <hasanboydesigner@gmail.com>
2026-07-19 12:45:51 +0500

    sabdkasdasdfdfdfd ddddddddddsdfsdasds wdssssssssssssssssscsddsasssssssss

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate, useNavigate } from 'react-router-dom';
 import { MobileLayout } from './components/Layout/MobileLayout';
 
 // Pages
